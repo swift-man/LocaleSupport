4ad4e7389445930d419f580a4461cae278a744d9
jiniopening <jiniopening@naver.com>
2022-08-09 23:39:54 +0900

    [Feat] create to locale support example

--- LocaleSupportExample/LocaleSupportExample/ViewController.swift
+++ LocaleSupportExample/LocaleSupportExample/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    let localeJP = Locale(localeIdentifiers: .japaneseJapan)
+    print(localeJP)
+    
+    let localeSupport = LocaleSupport()
+    let localeKR: Locale = localeSupport[.korean]
+    print(localeKR)
   }
-
-
 }
 
